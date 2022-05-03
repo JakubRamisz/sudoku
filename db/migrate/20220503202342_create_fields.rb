fd5f4c17e1d475be23d3a374c9142445ecfc97a5
Jakub Ramisz <jakubramisz01@gmail.com>
2022-05-03 22:38:39 +0200

    Add board and field model

---
+++ db/migrate/20220503202342_create_fields.rb
@@ -0,0 +1,9 @@
+class CreateFields < ActiveRecord::Migration[7.0]
+  def change
+    create_table :fields do |t|
+      t.references :board, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

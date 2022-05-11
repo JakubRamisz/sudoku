6a616afb9b5c4e06316f22b07f7ebc44e833ece7
Jakub Ramisz <jakubramisz01@gmail.com>
2022-05-11 23:51:30 +0200

    Add tests for board model

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class BoardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save board without name" do
+    board = Board.new
+    assert_not board.save, "Saved board without a name"
+  end
+
+  test "should create a solved board" do
+    board = Board.new(name: "test board")
+    assert board.save, "Did not save the board"
+
+    1.upto 9 do |number|
+      0.upto 8 do |i|
+        assert board.fields.where(row: i, value: number).size.eql?(1),
+               "Wrong number of #{number} in row #{i}"
+
+        assert board.fields.where(column: i, value: number).size.eql?(1),
+               "Wrong number of #{number} in column #{i}"
+      end
+
+      0.upto 2 do |i|
+        0.upto 2 do |j|
+          assert board.fields.where(row: 3*i..3*i+2, column: 3*j..3*j+2, value: number).size.eql?(1),
+                 "Wrong number of #{number} in box #{i} #{j}"
+        end
+      end
+    end
+
+
+  end
 end

2422aa2645c733bc12b05d70581cf4a89ffea779
Jakub Ramisz <jakubramisz01@gmail.com>
2022-05-26 19:55:36 +0200

    Add auto save on field deselect

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,2 +1,11 @@
 class FieldsController < ApplicationController
+  def update
+    @field = Field.find(params[:board_id])
+    @field.update(field_params)
+  end
+
+  private
+  def field_params
+    params.require(:field).permit(:value)
+  end
 end
